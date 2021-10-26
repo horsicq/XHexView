d28ace29fef9afe2fd6957979dc93daa69a8e599
horsicq <horsicq@gmail.com>
2021-10-26 22:58:31 +0200

    Fix: 2021-10-26

--- xhexview.cmake
+++ xhexview.cmake
@@ -1,6 +1,9 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
+include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialoggotoaddress.cmake)
+
 set(XHEXVIEW_SOURCES
+    ${DIALOGGOTOADDRESS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexview.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexview.ui
     ${CMAKE_CURRENT_LIST_DIR}/xhexview.cpp
