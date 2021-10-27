35a684b332b29683a7b429c04043785c24d63300
horsicq <horsicq@gmail.com>
2021-10-27 18:43:39 +0200

    Fix: 2021-10-27

--- xhexview.cmake
+++ xhexview.cmake
@@ -1,9 +1,27 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialoggotoaddress.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xabstracttableview.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xlineedithex.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialogdump.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialogsearch.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialoggotoaddress.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialoghexsignature.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../FormatWidgets/SearchSignatures/searchsignatureswidget.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XOptions/xoptions.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
 
 set(XHEXVIEW_SOURCES
     ${DIALOGGOTOADDRESS_SOURCES}
+    ${XABSTRACTTABLEVIEW_SOURCES}
+    ${XLINEEDITHEX_SOURCES}
+    ${DIALOGDUMP_SOURCES}
+    ${DIALOGSEARCH_SOURCES}
+    ${DIALOGGOTOADDRESS_SOURCES}
+    ${DIALOGHEXSIGNATURE_SOURCES}
+    ${SEARCHSIGNATURESWIDGET_SOURCES}
+    ${XOPTIONS_SOURCES}
+    ${FORMATS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexview.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexview.ui
     ${CMAKE_CURRENT_LIST_DIR}/xhexview.cpp
