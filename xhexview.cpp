e0c4301e0f431e57d9a3542bfa8d9cf348d9cf7c
horsicq <horsicq@gmail.com>
2022-02-13 01:28:30 +0100

    Fix: 2022-02-13

--- xhexview.cpp
+++ xhexview.cpp
@@ -269,7 +269,7 @@ void XHexView::updateData()
     }
 }
 
-void XHexView::paintCell(QPainter *pPainter, qint32 nRow, qint32 nColumn, qint32 nLeft, qint32 nTop, qint32 nWidth, qint32 nHeight)
+void XHexView::paintCell(QPainter *pPainter,qint32 nRow,qint32 nColumn,qint32 nLeft,qint32 nTop,qint32 nWidth,qint32 nHeight)
 {
     Q_UNUSED(nWidth)
 //    g_pPainterText->drawRect(nLeft,nTop,nWidth,nHeight);
