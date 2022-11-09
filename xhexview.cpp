69d38f57858e127f6e43d1fc4542be30276da0ce
horsicq <horsicq@gmail.com>
2022-11-10 00:32:46 +0100

    Fix: 2022-11-10

--- xhexview.cpp
+++ xhexview.cpp
@@ -50,15 +50,7 @@ XHexView::XHexView(QWidget *pParent) : XDeviceTableEditView(pParent)
 
 void XHexView::_adjustView()
 {
-    QFont _font;
-    QString sFont=getGlobalOptions()->getValue(XOptions::ID_HEX_FONT).toString();
-
-    if((sFont!="")&&_font.fromString(sFont))
-    {
-        setTextFont(_font);
-    }
-    // mb TODO errorString signal if invalid font
-    // TODO Check
+    setTextFontFromOptions(XOptions::ID_HEX_FONT);
 
     g_bIsAddressColon=getGlobalOptions()->getValue(XOptions::ID_HEX_ADDRESSCOLON).toBool();
 
