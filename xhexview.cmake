9fca0f584ac1101811570e397e33dd43029ac4b0
horsicq <horsicq@gmail.com>
2024-11-28 18:28:55 +0100

    Update file(s): 2024-11-28

--- xhexview.cmake
+++ xhexview.cmake
@@ -1,6 +1,10 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialoggotoaddress.cmake)
+if (NOT DEFINED DIALOGGOTOADDRESS_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialoggotoaddress.cmake)
+    set(XHEXVIEW_SOURCES ${XHEXVIEW_SOURCES} ${DIALOGGOTOADDRESS_SOURCES})
+endif()
+
 include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xabstracttableview.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xlineedithex.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialogdump.cmake)
@@ -19,15 +23,16 @@ include(${CMAKE_CURRENT_LIST_DIR}/../XHexEdit/xhexedit.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XSymbolsWidget/xsymbolswidget.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../die_widget/die_widget.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XVisualizationWidget/xvisualizationwidget.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialogwidget.cmake)
 
 set(XHEXVIEW_SOURCES
-    ${DIALOGGOTOADDRESS_SOURCES}
+    ${XHEXVIEW_SOURCES}
+    ${ABSTRACTWIDGETS_SOURCES}
     ${XABSTRACTTABLEVIEW_SOURCES}
     ${XLINEEDITHEX_SOURCES}
     ${DIALOGDUMP_SOURCES}
     ${DIALOGSEARCH_SOURCES}
     ${DIALOGSHOWDATA_SOURCES}
-    ${DIALOGGOTOADDRESS_SOURCES}
     ${DIALOGHEXSIGNATURE_SOURCES}
     ${DIALOGDATAINSPECTOR_SOURCES}
     ${SEARCHSIGNATURESWIDGET_SOURCES}
@@ -40,6 +45,11 @@ set(XHEXVIEW_SOURCES
     ${DIE_WIDGET_SOURCES}
     ${XDATACONVERTORWIDGET_SOURCES}
     ${XVISUALIZATIONWIDGET_SOURCES}
+    ${DIALOGWIDGET_SOURCES}
+)
+
+set(XHEXVIEW_SOURCES
+    ${XHEXVIEW_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexview.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexview.h
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexview.ui
